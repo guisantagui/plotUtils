5632a559e7da22ced42aa98f39ec352d77bf5965
guisantagui <gsantamaria@fc.ul.pt>
2025-01-21 12:41:12 +0100

    Add aes to imports.R and NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 
 #' @importFrom rlang sym ensym
 #' @importFrom factoextra facto_summarize
-#' @importFrom ggplot2 ggplot geom_point xlab ylab theme coord_fixed geom_segment
+#' @importFrom ggplot2 ggplot aes geom_point xlab ylab theme coord_fixed geom_segment
 #' @importFrom ggrepel geom_text_repel
 #' @importFrom ggpubr ggarrange
 #' @importFrom data.table data.table fread fwrite setcolorder
