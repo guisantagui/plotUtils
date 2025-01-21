06daa90a23f405a0853de99a559f870f25457b10
guisantagui <gsantamaria@fc.ul.pt>
2025-01-21 12:39:19 +0100

    Add ensym to imports.R and NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,6 @@
 # Imports
 
-#' @importFrom rlang sym
+#' @importFrom rlang sym ensym
 #' @importFrom factoextra facto_summarize
 #' @importFrom ggplot2 ggplot geom_point xlab ylab theme coord_fixed geom_segment
 #' @importFrom ggrepel geom_text_repel
